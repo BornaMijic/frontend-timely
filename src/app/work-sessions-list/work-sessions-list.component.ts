8f468735bcc0bdb22393d10daf9b875328cb22c2
Borna <borna.mijic0309@gmail.com>
2022-10-30 20:32:48 +0100

    add duration and showing current work session on list

--- src/app/work-sessions-list/work-sessions-list.component.ts
+++ src/app/work-sessions-list/work-sessions-list.component.ts
@@ -10,6 +10,8 @@ import {WorkSessionService} from "../home-page/work-session.service";
 })
 export class WorkSessionsListComponent implements OnInit, OnDestroy {
   workSessions: WorkSession[] = []
+  coutingStart: Date | null = null;
+  readonly DATE_FORMAT: string = 'dd-MM-yyyy hh:mm';
   private subscription: Subscription = new Subscription()
 
   constructor(private workSessionService: WorkSessionService) { }
@@ -21,10 +23,31 @@ export class WorkSessionsListComponent implements OnInit, OnDestroy {
       })
 
     this.subscription.add(subscription)
+
+    subscription = this.workSessionService.countingStartSubject.subscribe(
+      (startDate: Date) => {
+        this.coutingStart = startDate;
+      }
+    )
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+  }
+
+  getDuration(endDate: Date, startDate: Date): string {
+    let timeDiff: number = new Date(endDate).getTime() - new Date(startDate).getTime();
+    if(timeDiff == 0) {
+      return "00:00:00"
+    }
+    let seconds: string | number = Math.floor((timeDiff / 1000) % 60);
+    let minutes: string | number = Math.floor((timeDiff / (1000 * 60)) % 60);
+    let  hours: string | number = Math.floor((timeDiff / (1000 * 60 * 60)) % 24);
+
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+    return hours + ":" + minutes + ":" + seconds;
   }
 
 }
